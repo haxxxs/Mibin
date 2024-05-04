96358b122e1aca6610cd85411fa2d3fe7314c24a
St1ckySmash <99824309+St1ckySmash@users.noreply.github.com>
2024-05-04 12:53:31 +0400

    a

--- app/Models/Utilizator.php
+++ app/Models/Utilizator.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Utilizators extends Model
+class Utilizator extends Model
 {
     use HasFactory;
 }
