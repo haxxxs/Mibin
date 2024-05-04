c626cc07428991cdf0f71c68fddd178aac943fa8
St1ckySmash <99824309+St1ckySmash@users.noreply.github.com>
2024-05-04 18:24:53 +0400

    123

--- resources/views/trash_create.blade.php
+++ resources/views/trash_create.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=a4bec681-89bc-4176-bc7a-332f82e03309" type="text/javascript"></script>
+    <script src="http://127.0.0.1:8000/js/geolocation_control.js" type="module"></script>
+    <script src="https://yandex.st/jquery/2.2.3/jquery.min.js" type="text/javascript"></script>
 </head>
 <body>
 <div class="container">
@@ -42,6 +45,9 @@
 
                             <button type="submit">Submit</button>
                         </form>
+
+                        <div id="map" style="width: 600px; height: 400px"></div>
+
                     </div>
                 </div>
             </div>
